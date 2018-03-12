db0c6b86ab60f4648b303334d59a7ebd7e37b0f0
Kilahcim <krzysztof.michalik01@gmail.com>
2018-03-12 07:07:04 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$dynamicTitle = "Webmaster do Twoich usług.";
+$dynamicSubtitle = "Tworzenie aplikacji webowych to moja pasja.";
+include('./inc/header.html');
+include('./inc/about.html');
+include('./inc/projects.html');
+?>
+
+</body>
+</html>
